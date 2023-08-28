a7c833d6c8093698422b8fbb421984c06bbf3724
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-28 13:37:16 +0545

    submit and read data using Read incoming request approach

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -9,5 +9,17 @@ namespace Bloggie.Web.Controllers
         {
             return View();
         }
+
+        //go to the below method when submit the form 
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag()
+        {
+            //Two ways to submit and read data using Read incoming request approach
+
+            //var name = Request.Form["name"]; //Form["name"] -> name is the name from Add.cshtml input name
+            //var displayName = Request.Form["displayName"];
+            return View("Add");
+        }
     }
 }
